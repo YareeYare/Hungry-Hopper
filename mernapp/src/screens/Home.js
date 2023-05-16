4b48786748743d6cf734625526ac6006e5b51a15
YareeYare <mehulshakya230@gmail.com>
2023-05-16 16:12:23 +0530

    added footer and carousel to home page

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Card from '../Components/Card'
+import Carousel from '../Components/Carousel'
 
-export default function Home(){
+export default function Home() {
   return (
     <div>
-        <div> <Navbar></Navbar></div>
-        <div>Body</div>
-        <div> <Footer/> </div>
+      <div> <Navbar></Navbar></div>
+      <div> <Carousel/> </div>
+      <div>
+        <Card></Card>
+      </div>
+
+      <div> <Footer /> </div>
     </div>
   )
 }
