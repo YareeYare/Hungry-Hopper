ea58421d52b4b1f73aea2ff2361e6096df098017
YareeYare <mehulshakya230@gmail.com>
2023-05-16 13:41:05 +0530

    created components and screens

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Home from './screens/Home';
 
 function App() {
   return (
-    <div className='fs-1'>Hello World</div>
+    <>
+    <div> <Home></Home> </div>
+    </>
   );
 }
 
