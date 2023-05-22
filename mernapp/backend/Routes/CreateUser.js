033334f86131fd6bf49643a26b379cb0fac1c04b
YareeYare <mehulshakya230@gmail.com>
2023-05-23 05:15:24 +0530

    Added validator for signup page

--- mernapp/backend/Routes/CreateUser.js
+++ mernapp/backend/Routes/CreateUser.js
@@ -1,8 +1,21 @@
 const express =require('express')
 const router = express.Router()
 const User = require('../models/user')
+const { body , validationResult } = require('express-validator')
 
-router.post('/createuser', async (req,resp)=>{
+router.post('/createuser', 
+[
+    body('email','Invalid e-mail ID').isEmail(),
+    body('name','Name is too short, should be at least of 5 characters').isLength({ min : 5}),
+    body('password','Incorrect Password').isLength({ min : 4})
+]
+,async (req,resp)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return resp.status(400).json({ errors: errors.array() });
+    }
+
     try {
         await User.create({
             name : req.body.name,
