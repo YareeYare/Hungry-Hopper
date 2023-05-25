56799f90e594daf9e33d1a768222565725086aac
YareeYare <mehulshakya230@gmail.com>
2023-05-26 04:48:39 +0530

    added 'Add to Cart' option

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './screens/Home';
 import {
   BrowserRouter as Router,
@@ -8,12 +11,11 @@ import {
 } from "react-router-dom";
 import Login from './screens/Login';
 import Signup from './screens/Signup';
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import {CartProvider} from './Components/ContextReducer'
 
 function App() {
   return (
+    <CartProvider>
     <Router>
     <div>
       <Routes>
@@ -23,6 +25,7 @@ function App() {
       </Routes>
     </div>
     </Router>
+    </CartProvider>
   );
 }
 
--- mernapp/src/Components/Card.js
+++ mernapp/src/Components/Card.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect , useState , useRef} from "react";
+import { useDispatchCart , useCart } from "./ContextReducer";
 
 export default function Card(props) {
+
+let dispatch = useDispatchCart();
+
+let data = useCart();
+
+const priceRef = useRef();
 
   let options = props.options;
   let priceOptions = Object.keys(options)
 
+  const [qty, setQty] = useState(1)
+  const [size, setSize] = useState('')
+
+  const handleAddtoCart = async ()=>{
+    await dispatch({type : "ADD" , id : props.foodItem._id, name : props.foodItem.name, price: finalPrice, qty: qty, size: size})
+    console.log(data)
+  }
+
+let finalPrice = qty * parseInt(options[size]);
+
+useEffect( ()=>{
+  setSize(priceRef.current.value)
+}, [])
 
   return (
     <div>
@@ -13,12 +33,12 @@ export default function Card(props) {
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}
         >
-          <img src={props.imgSrc} className="card-img-top" alt="..." />
+          <img src={props.foodItem.img} className="card-img-top" alt="..." style={{height:'130px', objectFit:'fill'}}/>
           <div className="card-body">
-            <h5 className="card-title"> {props.foodname} </h5>
+            <h5 className="card-title"> {props.foodItem.name} </h5>
             <p className="card-text">Kuch bhi dish</p>
             <div className="container w-100">
-              <select className="m-2 h-100 bg-success rounded">
+              <select className="m-2 h-100 bg-success rounded" onChange={ (e)=> setQty(e.target.value) }>
                 {/* now some js part */}
                 {Array.from(Array(6), (e, i) => {
                   return (
@@ -29,17 +49,25 @@ export default function Card(props) {
                 })}
               </select>
 
-              <select className="m-2 h-100 bg-success rounded">
+              <select className="m-2 h-100 bg-success rounded" ref={priceRef} onChange={ (e)=> setSize(e.target.value) }>
                 {priceOptions.map((data)=>{
                   return <option key= {data} value = {data}> {data} </option>
                 })}
               </select> 
 
-              <div className="d-inline h-100 fs-5">Total Price</div>
+              <div className="d-inline h-100 fs-5">
+                ₹{finalPrice}/-
+              </div>
             </div>
+          
+          <hr/>
+          <button className={"btn btn-success justify-center ms-2"} onClick={handleAddtoCart}>Add to Cart</button>
+          
           </div>
+
         </div>
       </div>
+
     </div>
   );
 }
